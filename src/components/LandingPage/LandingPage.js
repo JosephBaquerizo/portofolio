0a6b59453ba383e498c2ca80ebe5bda43ce44bd3
JosephBaquerizo <61207703+JosephBaquerizo@users.noreply.github.com>
2020-05-20 15:21:30 -0500

    adding changes scroll X (responsive)

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -5,11 +5,11 @@ const LandingPage = () => {
 	return(
 		<div className='LPcontainer'>
 			<div className='LP' id='LPcontainer'>
-				<div className='LPtext'>
+				{/*<div className='LPtext'>
 					<p>Hi,</p>
 					<p className='word'>I'm Joseph,</p>
 					<p>Web Developer.</p>
-				</div>
+				</div>*/}
 			</div>
 			<div className='LPextra' id='AMcontainer'>
 			</div>
